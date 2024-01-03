4ab531ddd1aa4a2a4151684b32a7dbae0dc4fb43
Abhinav <abhinavkumar34002@gmail.com>
2024-01-03 15:36:05 +0530

    added api/validate_chirp endpoint

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"io"
+	// "log"
 	"net/http"
 
 	"github.com/Abhinav-6/chirpy/middleware"
@@ -32,8 +36,8 @@ func main() {
 	appRouter.Get("/healthz", healthzHandler)
 
 	adminRouter.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type","text/html")
-		fmt.Fprintf(w,`<html>
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, `<html>
 
 			<body>
 				<h1>Welcome, Chirpy Admin</h1>
@@ -45,6 +49,51 @@ func main() {
 
 	r.Mount("/app/", appRouter)
 	r.Mount("/admin", adminRouter)
+
+	r.Post("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		type param struct {
+			Body string `json:"body"`
+		}
+		type errorResponse struct {
+			Message string `json:"Error"`
+		}
+		type validResponse struct {
+			Message string `json:"Valid"`
+		}
+		dat, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "couldn't read request")
+			return
+		}
+		var params param
+		err = json.Unmarshal(dat, &params)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "error parsing request")
+			return
+		}
+		// decoder := json.NewDecoder(r.Body)
+		// params := param{}
+		// err := decoder.Decode(&params)
+		// if err != nil {
+		// 	// an error will be thrown if the JSON is invalid or has the wrong types
+		// 	// any missing fields will simply have their values in the struct set to their zero value
+		// 	log.Printf("Error decoding parameters: %s", err)
+		// 	w.WriteHeader(500)
+		// 	return
+		// }
+
+		if len(params.Body) > 140 {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, errorResponse{"Chirp is too long"}.Message)
+			return
+		}
+		w.WriteHeader(200)
+		fmt.Fprintf(w, validResponse{"OK"}.Message)
+
+	})
 
 	corsMux := middleware.MiddlewareCors(r)
 	http.ListenAndServe(":8080", corsMux)
