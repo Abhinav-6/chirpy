a8d9898e03bba4c7eb54d8016df271a91a422699
Abhinav <abhinavkumar34002@gmail.com>
2024-01-03 11:36:55 +0530

    added admin router

--- main.go
+++ main.go
@@ -19,21 +19,32 @@ func main() {
 	var config middleware.ApiConfig
 	r := chi.NewRouter()
 	appRouter := chi.NewRouter()
+	adminRouter := chi.NewRouter()
 	file := http.FileServer(http.Dir("."))
 	appRouter.Handle("/*", config.MiddlewareMetricsInc(http.StripPrefix("/app/", file)))
 	appRouter.Handle("/", config.MiddlewareMetricsInc(http.StripPrefix("/app", file)))
 
-	appRouter.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hits: %d", config.FileServerHits)
-	})
-
-	appRouter.Get("/reset", func(w http.ResponseWriter, r *http.Request) {
+	adminRouter.Get("/reset", func(w http.ResponseWriter, r *http.Request) {
 		config.FileServerHits = 0
-		http.Redirect(w, r, "/app/metrics", http.StatusPermanentRedirect)
+		http.Redirect(w, r, "/admin/metrics", http.StatusPermanentRedirect)
 	})
 
 	appRouter.Get("/healthz", healthzHandler)
+
+	adminRouter.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type","text/html")
+		fmt.Fprintf(w,`<html>
+
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+			
+			</html>`, config.FileServerHits)
+	})
+
 	r.Mount("/app/", appRouter)
+	r.Mount("/admin", adminRouter)
 
 	corsMux := middleware.MiddlewareCors(r)
 	http.ListenAndServe(":8080", corsMux)
